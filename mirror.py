c8451c1159c677ac882c654eb0c74d4dd9a9e111
tfunck <tffunck@gmail.com>
2023-07-03 08:29:07 -0400

    updated useage again

--- mirror.py
+++ mirror.py
@@ -12,19 +12,19 @@ if __name__ == '__main__':
 
 
     parser = argparse.ArgumentParser(description='Mirorr a volume.')
-    parser.add_argument('--input', '-i', dest='volume_fn', help='Filename of brain volume to mirror.')
-    parser.add_argument('--template', '-t', dest='template_fn', default=None, help='Optional filename of brain template to use as reference.')
-    parser.add_argument('--output', '-o', dest='out_fn', default=None, help='Output filename.')
+    parser.add_argument('input_filename', help='Filename of brain volume to mirror.')
+    parser.add_argument('output_filename', help='Output filename.')
+    parser.add_argument('--template', '-t', dest='template_filename', default=None, help='Optional filename of brain template to use as reference.')
     parser.add_argument('--offset', dest='offset',default=1000, help='Value to add to mirrored region to create unique mirrored ROI.')
     parser.add_argument('--clobber', dest='clobber',default=False,action='store_true', help='Overwrite existing results')
 
     args =parser.parse_args()
    
-    if not os.path.exists(args.out_fn) or args.clobber :
-        img = nib.load(args.volume_fn)
+    if not os.path.exists(args.out_filename) or args.clobber :
+        img = nib.load(args.input_filename)
 
-        if type(args.template_fn) != type(None):
-            affine = nib.load(args.template_fn).affine
+        if type(args.template_filename) != type(None):
+            affine = nib.load(args.template_filename).affine
         else:
             affine = img.affine
 
@@ -39,5 +39,5 @@ if __name__ == '__main__':
         x=np.rint((vol2.shape[0])/2).astype(int)
         vol[x+1:,:,:] = vol2[x:-1,:,:]
 
-        nib.Nifti1Image(vol, affine).to_filename(args.out_fn)
+        nib.Nifti1Image(vol, affine).to_filename(args.out_filename)
 
